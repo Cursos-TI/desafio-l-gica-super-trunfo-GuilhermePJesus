bb5b26fdabc9e3b374eb8348f8eb5cfea13f9ac1
Guilherme <guilhermedejesusperdigao@gmail.com>
2026-04-15 14:41:10 +0000

    Adicionei a exibição dos dados cadastrados das carta 1 e 2 e os resultados dos cálculos da Densidade Populacional, PIB per Capita e Super Poder.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -100,6 +100,31 @@ int main() {
 
     superpoder2 = populacao2 + area2 + pib2 + pontos2 + pibpercapita2 + densidade2;
 
+    // Mostrar cartas
+    printf("\n=== CARTA 1 ===\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos Turísticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f\n", densidade1);
+    printf("PIB per Capita: %.2f\n", pibpercapita1);
+    printf("Super Poder da Carta 1: %.2f\n", superpoder1);
+
+    printf("\n=== CARTA 2 ===\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos Turísticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f\n", densidade2);
+    printf("PIB per Capita: %.2f\n", pibpercapita2);
+    printf("Super Poder da Carta 2: %.2f\n", superpoder2);
+
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
